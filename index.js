8998eb9cf6bcc8932922a6ca6f3309b1bd3bd41b
Kris Walker <kris@kixx.name>
2017-11-17 10:46:16 -0500

    Start of v3 changes
     Changes to be committed:
    	modified:   .travis.yml
    	modified:   index.js
    	new file:   lib/http-client.js
    	new file:   lib/library.js
    	new file:   lib/management-client.js
    	new file:   package-lock.json
    	modified:   package.json

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-'use strict';
+const JWManagement = require(`./lib/management-client`);
+
+const client = JWManagement.createClient({
+	apiKey: `7P7fiutP`,
+	apiSharedSecret: `ioPr4TIErtUzuK3hRD21zg1T`
+});
+
+// function range(n) {
+// 	const a = [];
+// 	for (; n >= 0; n--) {
+// 		a.push(n);
+// 	}
+// 	return a;
+// }
+
+// const chunks = range(9).map(() => range(20));
+
+// const start = Date.now();
+
+// chunks.reduce((promise, chunk) => {
+// 	return promise.then(() => {
+// 		return Promise.all(chunk.map(() => {
+// 			return client(`/channels/list`, {result_limit: 10}).then(() => {
+// 				console.log(Date.now() - start);
+// 			});
+// 		}));
+// 	});
+// }, Promise.resolve(null));
+
+return client(`/channels/list`, {result_limit: 3});
